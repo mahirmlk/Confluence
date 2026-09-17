9b8573aeed4dc4219639113ad3af4f080f841799
mahirmlk <mahirmalikx@gmail.com>
2026-09-17 15:40:24 +0530

    chore(frontend): remove dead components and prune training barrel

--- frontend/src/components/training/index.ts
+++ frontend/src/components/training/index.ts
@@ -1,4 +1,2 @@
 export { TrainingPlayground } from "./TrainingPlayground";
-export { InteractiveConfusionMatrix } from "./InteractiveConfusionMatrix";
-export { InteractiveROCCurve, InteractivePRCurve } from "./InteractiveROCCurve";
 export { WrongPredictionExplorer } from "./WrongPredictionExplorer";
